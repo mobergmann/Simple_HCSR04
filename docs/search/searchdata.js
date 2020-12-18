9887e8e525814b7df886edec3c56c28fd1d96e1a
Moritz Bergmann <mobergmann@pm.me>
2020-12-18 13:52:44 +0100

    added documentation

---
+++ docs/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "cdgms",
+  1: "ms",
+  2: "s",
+  3: "cdgms",
+  4: "s"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
